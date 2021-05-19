00ff321041c1c1b6f4e6c8a1aa0adce134dea651
Adeleke Adeniji <adelekeadenijio@gmail.com>
2021-05-19 11:04:13 +0100

    Install SW

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,35 +1,27 @@
 importScripts("https://js.pusher.com/beams/service-worker.js");
 
-/*self.addEventListener('message', event => {
-  console.log('SW');
-  event.postMessage({message: 'Test from SW'})
-});*/
-
-self.addEventListener('message', event => {
-  console.log('Skipping');
-  if (event.data.action === 'skipWaiting') {
-    self.skipWaiting();
-  }
+self.addEventListener('install', event => {
+  console.log('Installed');
+  self.skipWaiting();
 });
 
-addEventListener('fetch', event => {
+self.addEventListener('activate', event => {
+  console.log('Activated');
+  // event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', event => {
   event.waitUntil(async function() {
     // Exit early if we don't have access to the client.
     // Eg, if it's cross-origin.
     if (!event.clientId) return;
-    console.log('event from SW: ', event);
-    console.log('event.clients: ', event.clients);
 
     // Get the client.
     const client = await clients.get(event.clientId);
     // Exit early if we don't get the client.
     // Eg, if it closed.
     if (!client) return;
-
-    /*await clients.matchAll().then(clients => {
-      console.log(`Got into the client.matchAll inner: ${clients}`);
-      clients.forEach(client => client.postMessage({msg: 'Hello from SW1'}));
-    })*/
+    console.log('!client: ', client);
 
     // Send a message to the client.
     PusherPushNotifications.onNotificationReceived = ({
@@ -41,21 +33,10 @@ addEventListener('fetch', event => {
       // This method triggers the default notification handling logic offered by
       // the Beams SDK. This gives you an opportunity to modify the payload.
       // E.g. payload.notification.title = "A client-determined title!"
-      // payload.notification.title = "A new title!";
-      console.log('After calling CLIENTS!!!')
+      console.log('After calling CLIENTS!!! payload: ', payload);
       client.postMessage(payload);
       pushEvent.waitUntil(handleNotification(payload));
-      /*pushEvent.waitUntil(
-        self.registration.showNotification(payload.notification.title, {
-          body: payload.notification.body,
-          icon: payload.notification.icon,
-          data: payload.data,
-        })
-      );*/
+      console.log('After ALL CLIENTS!!! payload: ', payload);
     }
   }());
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(clients.claim());
 });
--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import { ThemeProvider } from '@material-ui/core/styles';
 import theme from './theme';
 
+// Override the default scope of '/' with './', so that the registration applies
+// to the current directory and everything underneath it.
+
 ReactDOM.render(
     <ThemeProvider theme={theme}>
       {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
@@ -16,6 +19,13 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+window.addEventListener('load', () => {
+  navigator.serviceWorker.register('/service-worker.js').then(
+    registered => console.log('We got it registered!')
+  ).catch(err => {
+    console.log('Could not register SW: ', err)
+  });
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
